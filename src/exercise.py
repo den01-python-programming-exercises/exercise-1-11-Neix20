a8de51129d07e938de5c7f0098e509f86f566cb1
Neix20 <66442152+Neix20@users.noreply.github.com>
2020-06-08 16:55:50 +0800

    Attempt

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
-def main():
-    #write your code below this line
+def main():  # write your code below this line
+  msg = input("Give a string:")
+  msg2 = input("Give an integer:")
+  msg3 = input("Give a float:")
+  msg4 = input("Give a boolean:")
+  print("You gave the string "+msg)
+  print("You gave the integer "+msg2)
+  print("You gave the integer "+msg3)
+  print("You gave the integer "+msg4)
+
 
 if __name__ == '__main__':
-    main()
+   main()
